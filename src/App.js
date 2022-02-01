834700929ccec632e2ea33b44b50b42e00bae527
Rekss13 <rekss13@gmail.com>
2022-02-02 00:44:55 +0300

    code refactored and showing pressed buttons

--- src/App.js
+++ src/App.js
@@ -1,19 +1,9 @@
 import './App.css';
-import {CopyToClipboard} from 'pivotal-ui/react/copy-to-clipboard';
-import {PrimaryButton} from 'pivotal-ui/react/buttons';
-import {Icon} from 'pivotal-ui/react/iconography';
 import { hashtags } from './hashtags';
+import CopyButton from './CopyButton';
 
 const copyButtons = hashtags.map((element, index) => {
-    const indexString = index > 99 ? `${index + 1}` : index < 9 ? `00${index + 1}` : `0${index + 1}`;
-    const text = `Hashtags part ${indexString}, it's copied!`
-    return (
-        <div className="App-button">
-            <CopyToClipboard text={element} tooltip={text}>
-                <PrimaryButton large icon={<Icon src="copy" />} iconPosition="right">Part of # number {indexString}</PrimaryButton>
-            </CopyToClipboard>
-        </div>
-    );
+    return <CopyButton buttonNumber={index} copyText={element} key={index.toString()}/>
 });
 
 function App() {
